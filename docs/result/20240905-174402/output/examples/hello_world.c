a16697ceb6999fae03e5249cd923e6462d8fc587
d3fau1t <aciddust20@gmail.com>
2024-09-05 17:48:39 +0900

    fix: git ignore rules

---
+++ docs/result/20240905-174402/output/examples/hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void hello_banana() {
+    printf("Hello, Banana!\n");
+}
+
+void hello_pineapple() {
+    printf("Hello, Pineapple!\n");
+}
+
+int main() {
+    hello_banana();
+    hello_pineapple();
+    return 0;
+}
